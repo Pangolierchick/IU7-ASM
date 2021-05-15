be2b879fa69c1d5c12ce3df9cf23054ffeb49bff
Pangolierchick <gogofun@bk.ru>
2021-05-15 21:37:53 +0300

    benchmark fixes

--- lab_10/src/benchmark.cpp
+++ lab_10/src/benchmark.cpp
@@ -6,17 +6,17 @@ extern "C" {
 }
 
 static void bench_default_scalar(benchmark::State& state) {
-    const size_t iters = 10000;
-
-    vector_t v1 = new_avectorf32(8);
-    vector_t v2 = new_avectorf32(8);
+    vector_t v1 = new_avectorf32(72);
+    vector_t v2 = new_avectorf32(72);
 
     randomize_vector(v1);
     randomize_vector(v2);
 
     for (auto _ : state) {
-        for (size_t i = 0; i < iters; i++) {
-            auto res = scalar_product_vector(v1, v2);
+        while (state.KeepRunning()) {
+            benchmark::DoNotOptimize(
+                scalar_product_vector(v1, v2)
+            );
         }
     }
 
@@ -25,17 +25,17 @@ static void bench_default_scalar(benchmark::State& state) {
 }
 
 static void bench_sse_scalar(benchmark::State& state) {
-    const size_t iters = 10000;
-
-    vector_t v1 = new_avectorf32(8);
-    vector_t v2 = new_avectorf32(8);
+    vector_t v1 = new_avectorf32(72);
+    vector_t v2 = new_avectorf32(72);
 
     randomize_vector(v1);
     randomize_vector(v2);
 
     for (auto _ : state) {
-        for (size_t i = 0; i < iters; i++) {
-            auto res = scalar_product_sse_vector(v1, v2);
+        while (state.KeepRunning()) {
+            benchmark::DoNotOptimize(
+                scalar_product_sse_vector(v1, v2)
+            );
         }
     }
 
@@ -44,17 +44,17 @@ static void bench_sse_scalar(benchmark::State& state) {
 }
 
 static void bench_sse_scalar2(benchmark::State& state) {
-    const size_t iters = 10000;
-
-    vector_t v1 = new_avectorf32(8);
-    vector_t v2 = new_avectorf32(8);
+    vector_t v1 = new_avectorf32(72);
+    vector_t v2 = new_avectorf32(72);
 
     randomize_vector(v1);
     randomize_vector(v2);
 
     for (auto _ : state) {
-        for (size_t i = 0; i < iters; i++) {
-            auto res = scalar_product_sse_vector2(v1, v2);
+        while (state.KeepRunning()) {
+            benchmark::DoNotOptimize(
+                scalar_product_sse_vector2(v1, v2)
+            );
         }
     }
 
@@ -62,8 +62,8 @@ static void bench_sse_scalar2(benchmark::State& state) {
     destroy_vector(v2);
 }
 
-BENCHMARK(bench_default_scalar);
-BENCHMARK(bench_sse_scalar);
-BENCHMARK(bench_sse_scalar2);
+BENCHMARK(bench_default_scalar)->Arg(10000);
+BENCHMARK(bench_sse_scalar)->Arg(10000);
+BENCHMARK(bench_sse_scalar2)->Arg(10000);
 
 BENCHMARK_MAIN();
